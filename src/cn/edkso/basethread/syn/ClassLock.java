cee21430e2b9201b28cc710e73408bb1b93f0f43
dingmengdi-rui-7000 <1498047434@qq.com>
2021-07-10 23:13:31 +0800

    1.1.0.fix 线程基础=>synchronized锁：类所的本质=>修改结果说明和增加部分注释

--- src/cn/edkso/basethread/syn/ClassLock.java
+++ src/cn/edkso/basethread/syn/ClassLock.java
@@ -4,18 +4,21 @@ package cn.edkso.basethread.syn;
  * @author: dingmd
  * @create: 2021-07-10 22:28
  * @description: 类锁的本质
- *               结果：①阻塞类异常捕获后，需要在对异常做处理后（比如资源释放）通知中断（interrupt()）
- *                    ②类锁的本质是类的class对象，类的class对象在类加载时生成，一个类仅有有个class对象
- *                    ③锁对象（锁住的资源）如果是类的实例（对象）可能不唯一，锁住的不是同一个实例的时候锁无效
+ *
+ * 结果：①对象锁，若多个线程不是同一个对象锁（多个线程用的就不是同一把锁），锁无效
+ *              若多个线程是同一个对象锁（多个线程用的就是同一把锁），锁生效[本示例不体现]
+ *      ②类锁的本质是类的class对象，类的class对象在类加载时生成，一个类仅有有个class对象，
+ *             多个线程是同一个对象锁（多个线程用的就是同一把锁），锁生效
+ *      ③阻塞类异常捕获后，需要在对异常做处理后（比如资源释放）通知中断（interrupt()）
  **/
 public class ClassLock extends Thread{
 
 
     public void fun(){
         Object unique = new Object();
-        synchronized (unique){
-//        synchronized (Object.class){
-//        synchronized (unique.getClass()){
+        synchronized (unique){  //类锁
+//        synchronized (Object.class){  //类锁
+//        synchronized (unique.getClass()){ //类锁
             while (!isInterrupted()){
                 try {
                     Thread.sleep(500);
